6f2f5b4b166a027dbe933c1456f83015ccd5d6f5
sina sheikh <sinasheikhzahed@gmail.com>
2020-05-15 18:43:40 +0430

    Pulling objects from the database in Django, after Setting up admin panel & Creating model objects via the admin panel,

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Job
+
 
 # Create your views here.
-def elliot(request ):
-    return render(request, 'jobs/elliot.html')
+def elliot(request):
+    jobs_objects = Job.objects.all()
+    return render(request, 'jobs/elliot.html', {'jobs_list': jobs_objects})
